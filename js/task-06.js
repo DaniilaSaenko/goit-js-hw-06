407e8d82be10ffb4cf915e3016be7a4ea478d039
DaniilaSaenko <daniila.s777@gmail.com>
2022-11-14 20:01:23 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,16 @@
-const textInput = document.querySelector('#validation-input')
-/* console.log(textInput.getAttribute('data-length')) */
-textInput.addEventListener('blur', event => {
-	if (event.target.value.length == textInput.getAttribute('data-length')) {
-		textInput.classList.add('valid')
-		if (textInput.classList.contains('invalid')) {
-			textInput.classList.remove('invalid')
-		}
-	} else {
-		if (textInput.classList.contains('valid')) {
-			textInput.classList.remove('valid')
-		}
-		textInput.classList.add('invalid')
-	}
-})
+const textInput = document.querySelector("#validation-input");
+const controlValue = parseInt(textInput.getAttribute("data-length"), 10);
+
+textInput.addEventListener("blur", (event) => {
+  if (event.target.value.length === controlValue) {
+    textInput.classList.add("valid");
+    if (textInput.classList.contains("invalid")) {
+      textInput.classList.remove("invalid");
+    }
+  } else {
+    if (textInput.classList.contains("valid")) {
+      textInput.classList.remove("valid");
+    }
+    textInput.classList.add("invalid");
+  }
+});
